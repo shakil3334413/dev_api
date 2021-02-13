d88aed66188c928e0080216e45506a1add688533
shakil3334413 <shakil3334413@gmail.com>
2021-02-14 01:48:11 +0600

    Mongos Database Connect

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+
+const conectDB = async() => {
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: true
+    });
+
+    console.log(`Mongoes Connected ${conn.connection.host}`);
+}
+
+module.exports = conectDB
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 // const logger = require('./middleware/logger');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 //Routes Conncet 
 const bootcamps = require('./routes/bootcamps');
@@ -9,9 +10,12 @@ const bootcamps = require('./routes/bootcamps');
 //load env vars
 dotenv.config({ path: './config/config.env' });
 
+// connect Database
+connectDB();
+
 const app = express();
 
-//Dev Morgen middleware 
+//Dev Morgan middleware 
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -36,4 +40,16 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+    PORT,
+    console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+// Handle Unhandle promise Rejection
+
+process.on('unhandleRejection', (err, promise) => {
+    console.log(`Error:${err.message}`);
+    //close server and exit process
+    server.close(() => process.exit(1));
+
+});
